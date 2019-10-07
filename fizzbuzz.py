26f3080af9c5a1c49fe97220fb275cabe28bea7d
Ge0rg3 <16269580+Ge0rg3@users.noreply.github.com>
2019-10-07 16:41:24 +0100

    dumbass ur fizzbuzz is broken
    go watch some tom scott and come back https://www.youtube.com/watch?v=QPZ0pIK_wsc

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,10 @@
 for i in range(1,100):
-    fizzing = i % 3
-    buzzing = i % 5
-    if (fizzing & buzzing):
-        print("Fizzbuzz")
-    elif (fizzing):
-        print("Fizz")
-    elif (buzzing):
-        print("Buzz")
+    out = ""
+    if i%3 == 0:
+        out += "Fizz"
+    if i%5 == 0:
+        out += "Buzz"
+    if out:
+        print(out)
     else:
         print(i)
